1efc0a4bd5ebf9d9008e6882b6d5470149901950
Dominic Chang <doc.dominic@gmail.com>
2011-12-19 14:13:32 +0800

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  CurrencyConverter
+//
+//  Created by Dominic Chang on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Dominic Chang on 12/19/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
